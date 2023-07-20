0e5012a4fa0c9f28fa1bab42509a139e2af44789
Exxempt <46376436+Exxempt@users.noreply.github.com>
2023-07-20 14:34:15 -0500

    Update toggle dark - light theme button

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -12,31 +12,40 @@ import {
   } from "@/components/ui/dropdown-menu"
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { systemTheme, theme, setTheme } = useTheme()
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
+  // return (
+  //   <DropdownMenu>
+  //     <DropdownMenuTrigger asChild>
+  //       <Button variant="ghost" className="w-9 px-0">
+  //         <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+  //         <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
+  //         <span className="sr-only">Toggle theme</span>
+  //       </Button>
+  //     </DropdownMenuTrigger>
+  //     <DropdownMenuContent align="end">
+  //       <DropdownMenuItem onClick={() => setTheme("light")}>
+  //         <Sun className="mr-2 h-4 w-4" />
+  //         <span>Light</span>
+  //       </DropdownMenuItem>
+  //       <DropdownMenuItem onClick={() => setTheme("dark")}>
+  //         <Moon className="mr-2 h-4 w-4" />
+  //         <span>Dark</span>
+  //       </DropdownMenuItem>
+  //       {/* <DropdownMenuItem onClick={() => setTheme("system")}>
+  //         <Bot className="mr-2 h-4 w-4" />
+  //         <span>System</span>
+  //       </DropdownMenuItem> */}
+  //     </DropdownMenuContent>
+  //   </DropdownMenu>
+  // )
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="w-9 px-0">
+        <Button variant="ghost" className="w-9 px-0" onClick={() => theme == "dark"? setTheme('light'): setTheme("dark")}>
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
           <span className="sr-only">Toggle theme</span>
         </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          <Sun className="mr-2 h-4 w-4" />
-          <span>Light</span>
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          <Moon className="mr-2 h-4 w-4" />
-          <span>Dark</span>
-        </DropdownMenuItem>
-        {/* <DropdownMenuItem onClick={() => setTheme("system")}>
-          <Bot className="mr-2 h-4 w-4" />
-          <span>System</span>
-        </DropdownMenuItem> */}
-      </DropdownMenuContent>
-    </DropdownMenu>
   )
 }
