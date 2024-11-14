7630e1207737234c4ecd0018d8d9e3d88b113035
Exxempt <46376436+Exxempt@users.noreply.github.com>
2024-11-13 21:23:16 -0600

    package.json unfuck

--- contexts/Configurator.tsx
+++ contexts/Configurator.tsx
@@ -3,8 +3,6 @@
 import { createContext, useContext, useState, FC, PropsWithChildren, Dispatch, SetStateAction } from "react";
 
 interface ConfiguratorContext {
-    legs: string;
-    toggleLegs?: () => void;
     selectedClothesOption: string;
     selectedEarsOption: string;
     selectedEyesOption: string;
@@ -22,7 +20,6 @@ interface ConfiguratorContext {
 }
 
 const defaultConfigurator = {
-    legs: "0",
     selectedClothesOption: "None",
     selectedEarsOption: "None",
     selectedEyesOption: "None",
@@ -30,19 +27,18 @@ const defaultConfigurator = {
     selectedTypeOption: "Alien",
     selectedMouthOption: "None",
     selectedBGOption: "None",
-    setSelectedClothesOption: () => {}, 
-    setSelectedEarsOption: () => {}, 
-    setSelectedEyesOption: () => {}, 
-    setSelectedHatOption: () => {}, 
-    setSelectedMouthOption: () => {}, 
-    setSelectedTypeOption: () => {}, 
-    setSelectedBGOption: () => {}, 
+    setSelectedClothesOption: () => {},
+    setSelectedEarsOption: () => {},
+    setSelectedEyesOption: () => {},
+    setSelectedHatOption: () => {},
+    setSelectedMouthOption: () => {},
+    setSelectedTypeOption: () => {},
+    setSelectedBGOption: () => {},
 }
 
 const ConfiguratorContext = createContext<ConfiguratorContext>(defaultConfigurator);
 
 export const ConfiguratorProvider: FC<PropsWithChildren> = ({ children }) => {
-  const [legs, setLegs] = useState(defaultConfigurator.legs);
   const [selectedClothesOption, setSelectedClothesOption] = useState<string>(defaultConfigurator.selectedClothesOption);
   const [selectedEarsOption, setSelectedEarsOption] = useState<string>(defaultConfigurator.selectedEarsOption);
   const [selectedEyesOption, setSelectedEyesOption] = useState<string>(defaultConfigurator.selectedEyesOption);
@@ -51,15 +47,10 @@ export const ConfiguratorProvider: FC<PropsWithChildren> = ({ children }) => {
   const [selectedTypeOption, setSelectedTypeOption] = useState<string>(defaultConfigurator.selectedTypeOption);
   const [selectedBGOption, setSelectedBGOption] = useState<string>(defaultConfigurator.selectedBGOption);
 
-  const toggleLegs = () => {
-    setLegs(legs);
-  }
 
   return (
     <ConfiguratorContext.Provider
       value={{
-        legs,
-        toggleLegs,
         selectedClothesOption,
         selectedEarsOption,
         selectedEyesOption,
