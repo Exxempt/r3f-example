a98a989077a0c9fa454be8bdf05d799c40c54bb0
Exxempt <46376436+Exxempt@users.noreply.github.com>
2026-04-10 06:48:11 -0500

    Formatting

--- components/monke.tsx
+++ components/monke.tsx
@@ -42,7 +42,6 @@ export const Monke = () => {
         <Box position={[4, 3, 0]} color='#c3f3f4' />
         <Box position={[5, 3, 0]} color='#0e1a38' />
 
-
         <Box position={[-6, 2, 0]} color='#0e1a38' />
         <Box position={[-5, 2, 0]} color='#3585ba' />
         <Box position={[-4, 2, 0]} color='#4f9dc4' />
